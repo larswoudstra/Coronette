a0df17d549b39292dd310ff47e08a340aec4091b
Nina Alblas <ninaalblas@Ninas-MacBook-Pro.local>
2021-06-15 14:41:02 +0200

    missing the data

--- code/load_data.py
+++ code/load_data.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
+# load the data
 covid_train_data = pd.read_csv("/Users/ninaalblas/Documents/Minor AI/ML Project/Coronette/ml2021spring-hw1 (1)/covid.train.csv")
 print(covid_train_data.head())
+
+# check if there are any missing values
+if not covid_train_data.isnull().values.any():
+    print("Yay")
