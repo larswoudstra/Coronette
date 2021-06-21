e320edb6a1e8c90cdb7ee0ae9458f5dab4c7af43
lars woudstra <larswoudstra@MacBook-Pro-van-lars.local>
2021-06-21 13:39:10 +0200

    afbeeldingen voor feature importance

--- code/ml_code2.py
+++ code/ml_code2.py
@@ -42,7 +42,7 @@ def select_features(X_train, y_train, X_test, k={}):
     return X_train_best, X_test_best, feature_scores
 
 # select 'k' best features based on barplot, see images: 'best_features_barplot'
-k = 14
+k = 53
 
 train_k_best, test_k_best, feature_scores = select_features(train_data, train_targets.ravel(), test_data, k=k)
 
@@ -72,8 +72,8 @@ for train, val in kf.split(train_k_best):
     # - 93 input nodes
     # - 3 hidden layers (93, 60, and nodes, reLU activation)
     model.add(layers.Dense(units=k, activation='relu', input_shape=(k,)))
-    model.add(layers.Dense(units=round(k*(2/3)), activation='relu'))
-    model.add(layers.Dense(units=round(k*(1/3)), activation='relu'))
+    model.add(layers.Dense(units=round(k * (2/3)), activation='relu'))
+    model.add(layers.Dense(units=round(k * (1/3)), activation='relu'))
 
     # - 1 output node with a linear activation function
     model.add(layers.Dense(units=1))
